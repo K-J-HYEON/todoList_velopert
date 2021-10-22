349450c9bb754616d9c193e0ff2efb0cc23a918f
K-J-HYEON <kjh950601@naver.com>
2021-10-22 23:51:51 +0900

    first commit:

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,15 @@
+import { MdAddReaction } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = () => {
+  return (
+    <form className="TodoInsert">
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit">
+        <MdAddReaction />
+      </button>
+    </form>
+  );
+};
+
+export default TodoInsert;
